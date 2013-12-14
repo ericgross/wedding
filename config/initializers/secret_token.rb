4365550f7342ce7d4bd5f079c21cee161de9472c
Eric Gross <eric@globurg.com>
2013-12-14 15:44:28 -0500

    Add theme.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedding::Application.config.secret_key_base = '1d6745e955bf48d53c3545ee9f41505d4ba4ab784c91164151a43067bcf6e056953a0a9caf0027a4e2762df44722f1349e8c7e46bc04d7b3e1ecaa8d9b43cc86'
